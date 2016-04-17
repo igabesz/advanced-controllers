fdd8f77b51e85e1dc9927c6b943e8f011449df6c
Gábor IMRE <imre.gabesz@gmail.com>
2016-04-17 19:59:08 +0200

    feat(): can parse full body (#2), has better error management and extendable validation list

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -15,10 +15,28 @@ export declare function bodyString(name: string, optional?: boolean): (target: O
 export declare function bodyNumber(name: string, optional?: boolean): (target: Object, propertyKey: string | symbol, parameterIndex: number) => void;
 export declare function bodyObject(name: string, optional?: boolean): (target: Object, propertyKey: string | symbol, parameterIndex: number) => void;
 export declare function bodyArray(name: string, optional?: boolean): (target: Object, propertyKey: string | symbol, parameterIndex: number) => void;
+export declare var body: {
+    (type: any): (target: Object, propertyKey: string | symbol, parameterIndex: number) => void;
+    (name: string, type: any, optional?: boolean): (target: Object, propertyKey: string | symbol, parameterIndex: number) => void;
+};
 export declare function queryString(name: string, optional?: boolean): (target: Object, propertyKey: string | symbol, parameterIndex: number) => void;
 export declare function queryNumber(name: string, optional?: boolean): (target: Object, propertyKey: string | symbol, parameterIndex: number) => void;
 export declare function queryObject(name: string, optional?: boolean): (target: Object, propertyKey: string | symbol, parameterIndex: number) => void;
 export declare function queryArray(name: string, optional?: boolean): (target: Object, propertyKey: string | symbol, parameterIndex: number) => void;
+export declare function query(name: string, type: any, optional?: boolean): (target: Object, propertyKey: string | symbol, parameterIndex: number) => void;
+export interface Validator {
+    type: any;
+    check(value: any): boolean;
+    parse?(value: string): any;
+}
+export declare function addValidator(validator: Validator): void;
+export declare class WebError extends Error {
+    static requestErrorTransformer: (error: WebError, message: string, statusCode: number) => void;
+    statusCode: number;
+    text: string;
+    json: Object | Array<any>;
+    constructor(message: string, statusCode?: number);
+}
 export declare abstract class BaseController {
     register(app: express.Express, logger?: (level: 'debug' | 'error', message: string, meta: any) => void): void;
 }
--- src/index.ts
+++ src/index.ts
@@ -127,22 +127,90 @@ function addParamBinding(name: string, optional: boolean, from: string, type: an
 	};
 }
 
-export function bodyString(name: string, optional?: boolean) { return addParamBinding(name, optional, 'body', String); }
-export function bodyNumber(name: string, optional?: boolean) { return addParamBinding(name, optional, 'body', Number); }
-export function bodyObject(name: string, optional?: boolean) { return addParamBinding(name, optional, 'body', Object); }
-export function bodyArray(name: string, optional?: boolean) { return addParamBinding(name, optional, 'body', Array); }
+function obsWarn(oldAnnotation: string, newAnnotation: string) {
+	console.warn(`Obsolete ${oldAnnotation}, use ${newAnnotation} instead`);
+}
 
-export function queryString(name: string, optional?: boolean) { return addParamBinding(name, optional, 'query', String); }
-export function queryNumber(name: string, optional?: boolean) { return addParamBinding(name, optional, 'query', Number); }
-export function queryObject(name: string, optional?: boolean) { return addParamBinding(name, optional, 'query', Object); }
-export function queryArray(name: string, optional?: boolean) { return addParamBinding(name, optional, 'query', Array); }
+export function bodyString(name: string, optional?: boolean) { obsWarn('bodyString', 'body'); return addParamBinding(name, optional, 'body', String); }
+export function bodyNumber(name: string, optional?: boolean) { obsWarn('bodyNumber', 'body'); return addParamBinding(name, optional, 'body', Number); }
+export function bodyObject(name: string, optional?: boolean) { obsWarn('bodyObject', 'body'); return addParamBinding(name, optional, 'body', Object); }
+export function bodyArray(name: string, optional?: boolean) { obsWarn('bodyArray', 'body'); return addParamBinding(name, optional, 'body', Array); }
+export var body: {
+	/** Bind the whole request.body object */
+	(type: any): (target: Object, propertyKey: string | symbol, parameterIndex: number) => void;
+	/** Bind a member of the request.body object */
+	(name: string, type: any, optional?: boolean): (target: Object, propertyKey: string | symbol, parameterIndex: number) => void;
+} =
+function(name: string | any, type?: any, optional?: boolean) {
+	if (typeof name === 'string') {
+		return addParamBinding(name, optional, 'body', type);
+	}
+	return addParamBinding(null, false, 'full-body', type);
+};
+
+export function queryString(name: string, optional?: boolean) { obsWarn('queryString', 'query'); return addParamBinding(name, optional, 'query', String); }
+export function queryNumber(name: string, optional?: boolean) { obsWarn('queryNumber', 'query'); return addParamBinding(name, optional, 'query', Number); }
+export function queryObject(name: string, optional?: boolean) { obsWarn('queryObject', 'query'); return addParamBinding(name, optional, 'query', Object); }
+export function queryArray(name: string, optional?: boolean) { obsWarn('queryArray', 'query'); return addParamBinding(name, optional, 'query', Array); }
+export function query(name: string, type: any, optional?: boolean) { return addParamBinding(name, optional, 'query', type); }
+
+//------------------------------------------------------------------------------
+// Validation
+
+export interface Validator {
+	/** Type constructor */
+	type: any;
+	/** Property checker */
+	check(value: any): boolean;
+	/** Optional: for parsing items from query */
+	parse?(value: string): any;
+}
+
+let validators: Validator[] = [
+	{ type: String, check: _.isString, parse: input => input },
+	{ type: Number, check: _.isNumber, parse: parseInt },
+	{ type: Object, check: _.isObject, parse: JSON.parse },
+	{ type: Array, check: _.isArray, parse: JSON.parse },
+];
+
+export function addValidator(validator: Validator) {
+	if (_.some(validators, {type: validator.type})) {
+		throw new Error(`Cannot add validator with type ${validator.type}: already parsing that!`);
+	}
+	validators.push(validator);
+}
+
+//------------------------------------------------------------------------------
+// Error Handling
+
+export class WebError extends Error {
+	static requestErrorTransformer = (error: WebError, message: string, statusCode: number) => {
+		error.json = { errors: [ {message} ]};
+	};
+	statusCode: number;
+	text: string;
+	json: Object | Array<any>;
+
+	constructor(message: string, statusCode: number = 500) {
+		super(message);
+		this.statusCode = statusCode;
+		WebError.requestErrorTransformer(this, message, statusCode);
+	}
+}
+
+function handleError(err: Error | any, res: express.Response) {
+	err.statusCode = err.statusCode || 500;
+	if (err.json) {
+		return res.status(err.statusCode).json(err.json);
+	}
+	if (err.text) {
+		return res.status(err.statusCode).send(err.text);
+	}
+	return res.sendStatus(err.statusCode);
+}
 
 //------------------------------------------------------------------------------
 // Registering controller
-
-function close(res: Res, code: number, message?: string) {
-	res.status(code).send(message);
-}
 
 function registerControllerFunction(thisBind: any, app: express.Express, actionFunc: Function, logger: Function) {
 	var action = getHttpAction(actionFunc);
@@ -161,55 +229,89 @@ function registerControllerFunction(thisBind: any, app: express.Express, actionF
 		app.use(url, mwFunc.bind(thisBind));
 	}
 
+	// Creating parser functions
+	let binders = [];
+	let autoClose = true;
+	for (let bind of action.params) {
+		// Request
+		if (bind.from === 'req') {
+			binders.push((params: any[], req: Req, res: Res) => { params[bind.index] = req; });
+			continue;
+		}
+		// Response
+		if (bind.from === 'res') {
+			binders.push((params: any[], req: Req, res: Res) => { params[bind.index] = res; });
+			autoClose = false;
+			continue;
+		}
+		// Body or Query
+		let validator = validators.filter((item) => item.type === bind.type)[0];
+		if (!validator) throw new Error(`No validator for type: ${bind.type}`);
+		// Full-body: we MUST have body-parser here
+		if (bind.from === 'full-body') {
+			binders.push((params: any[], req: Req, res: Res) => {
+				// Cannot be optional
+				if (req.body === undefined) {
+					throw new WebError(`Empty Body`, 400);
+				}
+				if (!validator.check(req.body)) throw new Error(`Invalid value: Body should be a ${bind.type}`);
+				params[bind.index] = req.body;
+			});
+			continue;
+		}
+		// Body: we MUST have body-parser here
+		if (bind.from === 'body') {
+			binders.push((params: any[], req: Req, res: Res) => {
+				// It MUST be parsed
+				let parsed = req.body[bind.name];
+				if (parsed === undefined) {
+					if (!bind.opt) throw new WebError(`Missing property: ${bind.name}`, 400);
+					params[bind.index] = undefined;
+					return;
+				}
+				if (!validator.check(parsed)) throw new Error(`Invalid value: ${bind.name} should be a ${bind.type}`);
+				params[bind.index] = parsed;
+			});
+			continue;
+		}
+		// Query: we DON'T have body-parser here
+		else if (bind.from === 'query') {
+			if (!validator.parse) throw new Error(`No parser in validator for type: ${bind.type}; required when binding to query params`);
+			binders.push((params: any[], req: Req, res: Res) => {
+				let value = req.query[bind.name];
+				if (value === undefined) {
+					if (!bind.opt) throw new WebError(`Missing property: ${bind.name}`, 400);
+					params[bind.index] = undefined;
+					return;
+				}
+				let parsed = validator.parse(value);
+				if (!validator.check(parsed)) throw new Error(`Invalid value: ${bind.name} should be a ${bind.type}`);
+				params[bind.index] = parsed;
+			});
+		}
+	}
+
 	// Creating actionProcessor
 	var actionProcessor: (req: Req, res: Res) => any = (req: Req, res: Res) => {
 		let params = new Array(action.params.length);
-		let autoClose = true; // Send res.end() or res.json() after done
-		// Binding properties
-		for (let bind of action.params) {
-			// Body property
-			if (bind.from === 'body' || bind.from === 'query') {
-				var input = req[bind.from][bind.name];
-				if (input === undefined) {
-					if (bind.opt) continue;
-					return res.status(400).send(`Missing property: ${bind.name}`);
-				}
-				if (bind.from === 'query' && bind.type !== String) {
-					input = JSON.parse(input);
-				}
-				// Validating and saving
-				if ((bind.type === String) && !_.isString(input)) { return close(res, 400, `Invalid value: ${bind.name} should be a string`); }
-				if ((bind.type === Number) && !_.isNumber(input)) { return close(res, 400, `Invalid value: ${bind.name} should be a number`); }
-				if ((bind.type === Object) && !_.isObject(input)) { return close(res, 400, `Invalid value: ${bind.name} should be an object`); }
-				if ((bind.type === Array) && !_.isArray(input)) { return close(res, 400, `Invalid value: ${bind.name} should be an array`); }
-				params[bind.index] = input;
-			}
-			// Raw request
-			else if (bind.from === 'req') {
-				params[bind.index] = req;
-			}
-			// Raw response
-			else if (bind.from === 'res') {
-				params[bind.index] = res;
-				autoClose = false;
-			}
-		}
-		// Calling action
 		try {
+			// Applying binders
+			for (let binder of binders) binder(params, req, res);
+
+			// Calling the action
 			let result = actionFunc.apply(thisBind, params);
-			if (!autoClose)
-				return;
+			if (!autoClose) return;
 
 			// No result -> We're done
 			if (result === undefined)
-				return close(res, 200);
+				return res.sendStatus(200);
 			// Promise result -> Wait for it
 			else if (result instanceof Promise) {
 				(<any>result)
-				.then(response => (result !== undefined) ? res.json(response) : close(res, 200))
+				.then(response => (result !== undefined) ? res.json(response) : res.sendStatus(200))
 				.catch(ex => {
 					(!ex.statusCode) && logger && logger('error', 'Something broke (Promise)', { ex: ex, message: ex.message, stack: ex.stack });
-					close(res, ex.statusCode || 500);
+					handleError(ex, res);
 				});
 			}
 			// Anything else -> Send back as json
@@ -220,7 +322,7 @@ function registerControllerFunction(thisBind: any, app: express.Express, actionF
 		// Internal error
 		catch (ex) {
 			(!ex.statusCode) && logger && logger('error', 'Something broke (Exception)', { ex: ex, message: ex.message, stack: ex.stack });
-			close(res, ex.statusCode || 500);
+			handleError(ex, res);
 		}
 	};
 
@@ -260,3 +362,20 @@ export abstract class BaseController {
 		registerController(this, app, logger);
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
